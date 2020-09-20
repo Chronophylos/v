9aef2526a0bdf514fb4f0c453999b40139905a2a
Chronophylos <nikolai@chronophylos.com>
2020-09-20 19:33:40 +0200

    test stuff

--- tests/album.rs
+++ tests/album.rs
@@ -9,7 +9,7 @@ fn new() {
     let mut response = client
         .post("/a/new")
         .header(ContentType::Form)
-        .body("title=title&images=https%3A%2F%2Fi.imgur.com%2FVoyouQH.png")
+        .body("title=title&url=https%3A%2F%2Fi.imgur.com%2FVoyouQH.png")
         .dispatch();
 
     assert_eq!(response.status(), Status::Created);
@@ -24,11 +24,11 @@ fn new_invalid_image_url() {
     let mut response = client
         .post("/a/new")
         .header(ContentType::Form)
-        .body("title=title&images=https%3A%2F%2Fi.evil.com%2FVoyouQH.png")
+        .body("title=title&url=https%3A%2F%2Fi.evil.com%2FVoyouQH.png")
         .dispatch();
 
     assert_eq!(response.status(), Status::BadRequest);
-    assert_eq!(response.content_type(), Some(ContentType::HTML));
+    assert_eq!(response.content_type(), Some(ContentType::Plain));
     assert!(response.body().is_some());
 }
 
@@ -37,9 +37,9 @@ fn import() {
     let client = Client::new(v::rocket()).expect("valid rocket instance");
 
     let mut response = client
-        .post("/a/new")
+        .post("/a/import")
         .header(ContentType::Form)
-        .body("title=&link=https%3A%2F%2Fimgur.com%2Fa%2FJrheYnV")
+        .body("title=&url=https%3A%2F%2Fimgur.com%2Fa%2FJrheYnV")
         .dispatch();
 
     assert_eq!(response.status(), Status::Created);
@@ -68,8 +68,8 @@ fn head_non_existent_token() {
     let mut response = dbg!(client.head("/a/this_token_does_not_exist").dispatch());
 
     assert_eq!(response.status(), Status::NotFound);
-    assert_eq!(response.content_type(), None);
-    assert_eq!(response.body_string(), None);
+    assert_eq!(response.content_type(), Some(ContentType::Plain));
+    assert_eq!(response.body_string(), Some("".to_string()));
 }
 
 #[test]
@@ -80,4 +80,18 @@ fn token_unsupported_methods() {
     for req in vec![client.delete(url), client.options(url), client.put(url)] {
         assert_eq!(req.dispatch().status(), Status::NotFound);
     }
+}
+
+#[test]
+#[ignore = "not final, missing api to check if ressouces were correctly added"]
+fn create_album_and_insert() {
+    let client = Client::new(v::rocket()).expect("valid rocket instance");
+
+    let response = client
+        .post("/a/new")
+        .header(ContentType::Form)
+        .body("title=title&url=https%3A%2F%2Fi.imgur.com%2FVoyouQH.png")
+        .dispatch();
+
+    assert_eq!(response.status(), Status::Created);
 }
